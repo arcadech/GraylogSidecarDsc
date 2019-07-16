e25748992f5945551caa0b7e47d32c32342d4727
Claudio Spizzi <claudio@spizzi.net>
2019-07-16 14:53:56 +0200

    Update test cases

--- Modules/GraylogSidecarDsc/Tests/ARCADE_GraylogSidecar.Tests.ps1
+++ Modules/GraylogSidecarDsc/Tests/ARCADE_GraylogSidecar.Tests.ps1
@@ -34,7 +34,7 @@ InModuleScope 'ARCADE_GraylogSidecar' {
         $getParams = @{
             IsSingleInstance = 'Yes'
             SetupPath        = "$Env:Temp\graylog_sidecar_installer_1.0.1-1.exe"
-            ServerUrl        = 'https://graylog.contoso.com/api/'
+            ServerUrl        = 'https://graylog.contoso.com/'
             ServerApiToken   = 'yb34rs6w9nmd6p5y3x2a4kk6a7n8q9p9w7d5us4de3l2ahg'
         }
 
@@ -54,7 +54,7 @@ InModuleScope 'ARCADE_GraylogSidecar' {
             IsSingleInstance = 'Yes'
             Ensure           = 'Present'
             SetupPath        = "$Env:Temp\graylog_sidecar_installer_1.0.1-1.exe"
-            ServerUrl        = 'https://graylog.contoso.com/api/'
+            ServerUrl        = 'https://graylog.contoso.com/'
             ServerApiToken   = 'yb34rs6w9nmd6p5y3x2a4kk6a7n8q9p9w7d5us4de3l2ahg'
             NodeName         = 'demo'
         }
@@ -75,7 +75,7 @@ InModuleScope 'ARCADE_GraylogSidecar' {
             IsSingleInstance = 'Yes'
             Ensure           = 'Present'
             SetupPath        = "$Env:Temp\graylog_sidecar_installer_1.0.1-1.exe"
-            ServerUrl        = 'https://graylog.contoso.com/api/'
+            ServerUrl        = 'https://graylog.contoso.com/'
             ServerApiToken   = 'yb34rs6w9nmd6p5y3x2a4kk6a7n8q9p9w7d5us4de3l2ahg'
             NodeName         = 'demo'
         }
