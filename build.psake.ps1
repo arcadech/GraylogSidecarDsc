81c86f489b34c75cca7ea69fcb591356e4fa1936
Claudio Spizzi <claudio@spizzi.net>
2019-09-20 14:15:34 +0200

    Update build script

--- build.psake.ps1
+++ build.psake.ps1
@@ -86,6 +86,9 @@ param ()
 
 ## Configuration and Default task
 
+# Load project configuration
+. "$PSScriptRoot\build.settings.ps1"
+
 # Default build configuration
 Properties {
 
@@ -126,9 +129,6 @@ Properties {
     $GitHubRepoName      = ''
     $GitHubToken         = ''
 }
-
-# Load project configuration
-. $PSScriptRoot\build.settings.ps1
 
 # Default task
 Task Default -depends Build, Test
@@ -529,12 +529,6 @@ function Test-GitRepo($ModuleName)
     if ($gitStatus.Branch -ne 'master')
     {
         throw "Git Exception: $($gitStatus.Branch) is checked out, switch to master branch!  (git checkout master)"
-    }
-
-    $mergeStatus = Get-GitMergeStatus -Branch 'master'
-    if ($mergeStatus -notcontains 'dev')
-    {
-        throw "Git Exception: dev branch is not merged into the master branch!  (git merge dev)"
     }
 
     if ($gitStatus.AheadBy -ne 0)
